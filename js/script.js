085c5de7fe66dc438f20bf103025a8181dc39e7c
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-15 17:23:25 +0100

    add. js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+// jquery ready
 
+$(function( ){
+
+  // al click sui link del right menu
+  $(".show-menu").click(function() {
+
+    var showBtn = $(".show-menu");
+
+    showBtn.next().removeClass("active");
+
+    $(this).next().toggleClass("active");
+
+
+  });
+
+
+
+
+
+
+
+
+});
+
+// jquery ready
